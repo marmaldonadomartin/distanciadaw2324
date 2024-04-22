097bbc271e0bc6cdb4b8df6a93d85adb9351ad8e
Mar <mar.maldonado.martin@gmail.com>
2024-04-22 10:44:16 +0200

    Versión 1.0

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Script para practicar PHPDocumentor.
+ *
+ * Este script contiene funciones que realizan operaciones simples para documentar con PHPDocumentor.
+ *
+ * @package Practica_maldonado
+ * @version 1.0
+ * @author Maria del Mar Maldonado Martin
+ */
+
+/**
+ * Función para sumar dos números.
+ *
+ * Esta función toma dos números y devuelve su suma.
+ *
+ * @param int $num1 El primer número.
+ * @param int $num2 El segundo número.
+ * @return int La suma de los dos números.
+ */
+function sumar($num1, $num2) {
+    return $num1 + $num2;
+}
+
+/**
+ * Función para multiplicar dos números.
+ *
+ * Esta función toma dos números y devuelve su producto.
+ *
+ * @param int $num1 El primer número.
+ * @param int $num2 El segundo número.
+ * @return int El producto de los dos números.
+ */
+function multiplicar($num1, $num2) {
+    return $num1 * $num2;
+}
+
+/**
+ * Anotación para desarrolladores:
+ *
+ * Módulo Desarrollo de Aplicaciones Web. Curso 2023/2024
+ */
+?>
